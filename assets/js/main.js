933d77a75698b7a53807f098e05012d761f8d9c7
shchetinin <45269644+Vlasdislav@users.noreply.github.com>
2022-08-07 21:54:47 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,16 +17,3 @@ const creteCardProduct = (data) => {
 getData(`./assets/db/products-info.json`).then((data) => {
     data.forEach(creteCardProduct)
 });
-
-const choose_page = () => {
-    const more_buttons = document.querySelectorAll('.product-item-more');
-    more_buttons.forEach(elem => {
-        elem.addEventListener('click', event => {
-            const target = event.target;
-            const id = target.closest('.product-item').dataset.id;
-            localStorage.setItem('product-id', id);
-        });
-    });
-}
-
-setInterval(choose_page, 1000);
